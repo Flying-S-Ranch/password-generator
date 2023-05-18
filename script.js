4dfdaf8c9753db07ddac062bc13bfbdda158d175
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-05-18 16:59:26 -0500

    adds pseudo-code

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-// Assignment Code
+// DEPENDENCIES
 var generateBtn = document.querySelector("#generate");
+
+// DATA
+
+//FUNCTIONS
+
+
+function generatePassword() {
+  console.log("generating password");
+  //Ask user how long they want their password to be
+  //Store user length within a variable
+  //Ask user what type of characters they want to use
+  //Store user characters within a variable
+  //Use chosen length and characters to make a password
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword()
+
+  // CREATE
   var passwordText = document.querySelector("#password");
-
+  // BUILD
   passwordText.value = password;
-
+  // PLACE
 }
 
-// Add event listener to generate button
+// USER INTERACTIONS
 generateBtn.addEventListener("click", writePassword);
